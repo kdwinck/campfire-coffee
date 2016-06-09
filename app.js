ea5f1ecdb5bcd8a58573d9c363d57b2ab35e099b
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-08 18:17:35 -0700

    converted pikePlace object literal to constructor object

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // create Pike Place Coffee shop
-var pikePlace = {
+function CoffeStand(name, minCust, maxCust, cupsPerCust, poundsPerCust) {
 
-  name: 'Pike Place Market',
-  hours: ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'],
-  minCust: 14,
-  maxCust: 35,
-  cupsPerCust: 1.2,
-  poundsPerCust: .34,
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.cupsPerCust = cupsPerCust;
+  this.poundsPerCust = poundsPerCust;
 
-  totalCustomers: 0,
-  totalCups: 0,
-  packagedPounds: 0,
-  dailyPounds: 0,
+  this.hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+  this.totalCustomers = 0;
+  this.totalCups = 0;
+  this.packagedPounds = 0;
+  this.dailyPounds = 0;
 
-  custPerHour: [],
-  cupsPerHour: [],
-  poundsPerHour: [],
-  cupsPerPound: [],
-  employeesNeeded: [],
-  totalPoundsPerHour: [],
+  this.custPerHour = [];
+  this.cupsPerHour = [];
+  this.poundsPerHour = [];
+  this.cupsPerPound = [];
+  this.employeesNeeded = [];
+  this.totalPoundsPerHour = [];
 
-  avgCustPerHour: function() {
+  this.avgCustPerHour = function() {
     for (value in this.hours) {
       var customers = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
       this.custPerHour.push(customers);
       this.totalCustomers += customers;
     }
-  },
+  };
 
-  calcHourlyValues: function() {
+  this.calcHourlyValues = function() {
     for (value in this.custPerHour) {
       // calculate cups of coffee sold per hour
       var cupsPerHour = Math.round((this.custPerHour[value] * this.cupsPerCust) * 10) / 10;
@@ -42,47 +42,47 @@ var pikePlace = {
       var cupsInPounds = Math.round((this.cupsPerHour[value] / 16) * 10) / 10;
       this.cupsPerPound.push(cupsInPounds);
     }
-  },
+  };
 
-  calcEmployeesPerHour: function() {
+  this.calcEmployeesPerHour = function() {
     for (value in this.custPerHour) {
       var employees = Math.ceil(this.custPerHour[value] / 30);
       this.employeesNeeded.push(employees);
     }
-  },
+  };
 
-  calcTotalPoundsPerHour: function() {
+  this.calcTotalPoundsPerHour = function() {
     for (value in this.hours) {
       var hourlyPounds = Math.round((this.cupsPerPound[value] + this.poundsPerHour[value]) * 10 ) / 10;
       this.totalPoundsPerHour.push(hourlyPounds);
       this.dailyPounds += hourlyPounds;
     }
-  },
+  };
 
-  displayName: function() {
+  this.displayName = function() {
     var main = document.getElementById('main');
     var storeName = document.createElement('p');
     storeName.textContent = this.name;
     main.appendChild(storeName);
-  },
+  };
 
-  createList: function() {
+  this.createList = function() {
     var main = document.getElementById('main');
     var ul_tag = document.createElement('ul');
     ul_tag.id = 'pike';
     main.appendChild(ul_tag);
-  },
+  };
 
-  createListItems: function() {
+  this.createListItems = function() {
     var ul_tag = document.getElementById('pike');
     for (value in this.hours) {
       var listData = document.createElement('li');
       listData.textContent = this.hours[value] + ': ' + this.totalPoundsPerHour[value] + ' lbs [' + this.custPerHour[value] + ' customers, ' + this.cupsPerHour[value] + ' cups (' + this.cupsPerPound[value] + ' lbs), ' + this.poundsPerHour[value] + ' lbs-to-go]',
       ul_tag.appendChild(listData);
     }
-  },
+  };
 
-  createListTotals: function() {
+  this.createListTotals = function() {
     var ul_tag = document.getElementById('pike');
     var listData = document.createElement('li');
     // append total custsomers
@@ -100,9 +100,9 @@ var pikePlace = {
     listData = document.createElement('li');
     listData.textContent = 'Total Pounds of beans needed at ' + this.name + ': ' + (this.dailyPounds).toFixed(1);
     ul_tag.appendChild(listData);
-  },
+  };
 
-  theBigOne: function() {
+  this.theBigOne = function() {
     this.avgCustPerHour();
     this.calcHourlyValues();
     // this.calcPoundsPerHour();  added these to calcHourlyValues method
@@ -113,9 +113,13 @@ var pikePlace = {
     this.createList();
     this.createListItems();
     this.createListTotals();
-  }
-};
+  };
+}
 
+var pikePlace = new CoffeStand('Pike Place Market', 14, 35, 1.2, .34);
+
+
+pikePlace.theBigOne();
 
 var capHill = {
 
@@ -580,7 +584,7 @@ var seaTac = {
   }
 };
 
-pikePlace.theBigOne();
+// pikePlace.theBigOne();
 capHill.theBigOne();
 seattleLibrary.theBigOne();
 southLakeUnion.theBigOne();
