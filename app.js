9979f18cfae6b4319db88ca47b99e7b63c15f2ba
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-09 17:17:18 -0700

    created function to make Totals row of coffee table and added table border

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // create global variable for hours of operation
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+var allStoresDailyPounds = 0;
+var allStoresHourlyPounds = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // create constructor function to build each coffee stand
 function CoffeeStand(name, minCust, maxCust, cupsPerCust, poundsPerCust, hours) {
-
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -63,51 +64,53 @@ CoffeeStand.prototype.calcTotalPoundsPerHour = function() {
     var hourlyPounds = Math.round((this.cupsPerPound[index] + this.poundsPerHour[index]) * 10 ) / 10;
     this.totalPoundsPerHour.push(hourlyPounds);
     this.dailyPounds += hourlyPounds;
+    allStoresHourlyPounds[index] += hourlyPounds;
   }
+  allStoresDailyPounds += this.dailyPounds;
 };
 
-CoffeeStand.prototype.displayName = function() {
-  var main = document.getElementById('main');
-  var storeName = document.createElement('p');
-  storeName.textContent = this.name;
-  main.appendChild(storeName);
-};
-
-CoffeeStand.prototype.createList = function() {
-  var main = document.getElementById('main');
-  var ul_tag = document.createElement('ul');
-  ul_tag.id = this.name;
-  main.appendChild(ul_tag);
-};
-
-CoffeeStand.prototype.createListItems = function() {
-  var ul_tag = document.getElementById(this.name);
-  for (value in this.hours) {
-    var listData = document.createElement('li');
-    listData.textContent = this.hours[value] + ': ' + this.totalPoundsPerHour[value] + ' lbs [' + this.custPerHour[value] + ' customers, ' + this.cupsPerHour[value] + ' cups (' + this.cupsPerPound[value] + ' lbs), ' + this.poundsPerHour[value] + ' lbs-to-go]',
-    ul_tag.appendChild(listData);
-  }
-};
-
-CoffeeStand.prototype.createListTotals = function() {
-  var ul_tag = document.getElementById(this.name);
-  var listData = document.createElement('li');
-  // append total custsomers
-  listData.textContent = 'Total customers at ' + this.name + ': ' + Math.ceil(this.totalCustomers);
-  ul_tag.appendChild(listData);
-  // append total cups
-  listData = document.createElement('li');
-  listData.textContent = 'Total cups sold at ' + this.name + ': ' + Math.ceil(this.totalCups);
-  ul_tag.appendChild(listData);
-  // append total packaged pounds
-  listData = document.createElement('li');
-  listData.textContent = 'Total to-go pound packages sold at ' + this.name + ': ' + (this.packagedPounds).toFixed(1);
-  ul_tag.appendChild(listData);
-  // append daily pounds total
-  listData = document.createElement('li');
-  listData.textContent = 'Total pounds of beans needed at ' + this.name + ': ' + (this.dailyPounds).toFixed(1);
-  ul_tag.appendChild(listData);
-};
+// CoffeeStand.prototype.displayName = function() {
+//   var main = document.getElementById('main');
+//   var storeName = document.createElement('p');
+//   storeName.textContent = this.name;
+//   main.appendChild(storeName);
+// };
+//
+// CoffeeStand.prototype.createList = function() {
+//   var main = document.getElementById('main');
+//   var ul_tag = document.createElement('ul');
+//   ul_tag.id = this.name;
+//   main.appendChild(ul_tag);
+// };
+//
+// CoffeeStand.prototype.createListItems = function() {
+//   var ul_tag = document.getElementById(this.name);
+//   for (value in this.hours) {
+//     var listData = document.createElement('li');
+//     listData.textContent = this.hours[value] + ': ' + this.totalPoundsPerHour[value] + ' lbs [' + this.custPerHour[value] + ' customers, ' + this.cupsPerHour[value] + ' cups (' + this.cupsPerPound[value] + ' lbs), ' + this.poundsPerHour[value] + ' lbs-to-go]',
+//     ul_tag.appendChild(listData);
+//   }
+// };
+//
+// CoffeeStand.prototype.createListTotals = function() {
+//   var ul_tag = document.getElementById(this.name);
+//   var listData = document.createElement('li');
+//   // append total custsomers
+//   listData.textContent = 'Total customers at ' + this.name + ': ' + Math.ceil(this.totalCustomers);
+//   ul_tag.appendChild(listData);
+//   // append total cups
+//   listData = document.createElement('li');
+//   listData.textContent = 'Total cups sold at ' + this.name + ': ' + Math.ceil(this.totalCups);
+//   ul_tag.appendChild(listData);
+//   // append total packaged pounds
+//   listData = document.createElement('li');
+//   listData.textContent = 'Total to-go pound packages sold at ' + this.name + ': ' + (this.packagedPounds).toFixed(1);
+//   ul_tag.appendChild(listData);
+//   // append daily pounds total
+//   listData = document.createElement('li');
+//   listData.textContent = 'Total pounds of beans needed at ' + this.name + ': ' + (this.dailyPounds).toFixed(1);
+//   ul_tag.appendChild(listData);
+// };
 
 CoffeeStand.prototype.createCoffeeRow = function () {
   var table = document.getElementById('coffeeTable');
@@ -160,7 +163,7 @@ function createCoffeeTable() {
 // create function to make table header row
 function createCoffeeHeader() {
   var table = document.getElementById('coffeeTable');
-  var header = document.createElement('thead');
+  var header = document.createElement('tr');
   table.appendChild(header);        // make table header row
   var data = document.createElement('td');
   data.textContent = '';
@@ -175,11 +178,33 @@ function createCoffeeHeader() {
   }
 };
 
+function createCoffeeTotalsRow() {
+  var table = document.getElementById('coffeeTable');
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.textContent = 'Totals';
+  row.appendChild(cell);
+  cell = document.createElement('td');
+  cell.textContent = Math.round(allStoresDailyPounds * 10) / 10;
+  row.appendChild(cell);
+  for (var index in allStoresHourlyPounds) {
+    cell = document.createElement('td');
+    cell.textContent = Math.round(allStoresHourlyPounds[index] * 10) / 10;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+
 createCoffeeTable(); //create empty table
-createCoffeeHeader(); //create header row
+createCoffeeHeader(); //create header ro
 
 pikePlace.theBigOne();
 capHill.theBigOne();
 seattleLibrary.theBigOne();
 southLakeUnion.theBigOne();
 seaTac.theBigOne();
+
+console.log(allStoresDailyPounds);
+console.log(allStoresHourlyPounds);
+
+createCoffeeTotalsRow(); //create coffee table totals row
